0a6ba4d5153bff534c9210af9aa2d2ba2c5c944e
KMagdi <46417302+kmagdi@users.noreply.github.com>
2021-11-20 19:58:03 +0100

    clickCounter correction

--- src/App.js
+++ src/App.js
@@ -1,92 +1,93 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from "./components/Board"
 import init from "./components/init"
-import  "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "./App.css"
 
 import countapi from 'countapi-js';
 
 countapi.visits().then((result) => {
-    console.log(" Az oldal látogatottsági mutatója:"+result.value);
+    console.log(" Az oldal látogatottsági mutatója:" + result.value);
 });
 
-export const cardNumber= [2, 4, 6,8,10];
+export const cardNumber = [2, 4, 6, 8, 10];
 
-export const App=()=>{
-    const [gameSize,setGameSize]=useState(cardNumber[1])
-    const [cards,setCards]=useState([])
-    const [flipped,setFlipped]=useState([])
-    const [solved,setSolved]=useState([])
-    const [disabled,setDisabled]=useState(false) //hogy a felhasznalo ne kattinthasson tobbszor ugyanarra a kartyara
-    const [clickCounter,setClickCounter]=useState(0)
-    const [dimension,setDimension]=useState(400)
+export const App = () => {
+    const [gameSize, setGameSize] = useState(cardNumber[1])
+    const [cards, setCards] = useState([])
+    const [flipped, setFlipped] = useState([])
+    const [solved, setSolved] = useState([])
+    const [disabled, setDisabled] = useState(false) //hogy a felhasznalo ne kattinthasson tobbszor ugyanarra a kartyara
+    const [clickCounter, setClickCounter] = useState(0)
+    const [dimension, setDimension] = useState(400)
 
-    useEffect(()=>{
+    useEffect(() => {
         setSolved([])
         resizeBoard()
+        setClickCounter(0)
         setCards(init(gameSize))
-    },[gameSize])//ha ures arrayt adunk itt meg akkor csak legeloszor egyszer hivodik meg a useEFFECT
+    }, [gameSize])//ha ures arrayt adunk itt meg akkor csak legeloszor egyszer hivodik meg a useEFFECT
     //igy ahanyszor valtozik a gameSize annyiszor fog meghivodni itt a useEffect
 
-useEffect(()=>{
-    const resizeListener=window.addEventListener('resize',resizeBoard)
-    return ()=>window.removeEventListener('resize',resizeListener)
-})
+    useEffect(() => {
+        const resizeListener = window.addEventListener('resize', resizeBoard)
+        return () => window.removeEventListener('resize', resizeListener)
+    })
 
-const resizeBoard=()=>{
-    setDimension(Math.min(
-        document.documentElement.clientHeight,
-        document.documentElement.clientWidth
-    ))
-}
+    const resizeBoard = () => {
+        setDimension(Math.min(
+            document.documentElement.clientHeight,
+            document.documentElement.clientWidth
+        ))
+    }
 
-useEffect(()=>{
-    preloadImages()
-},[])//annyiszor hivodik meg ahanyszor a cards valtozik
+    useEffect(() => {
+        preloadImages()
+    }, [])//annyiszor hivodik meg ahanyszor a cards valtozik
 
-    const handleClick=(id)=>{
+    const handleClick = (id) => {
         setDisabled(true)
-        if(flipped.length===0){
+        if (flipped.length === 0) {
             setFlipped([id])
             setDisabled(false)
-        } else{
-            if(sameCard(id)) return
-            setFlipped([flipped[0],id])
+        } else {
+            if (sameCard(id)) return
+            setFlipped([flipped[0], id])
 
-            if(isMatch(id)){
-                setSolved([...solved,flipped[0],id])
-               resetCards()
-            }else{
-                setClickCounter(clickCounter+1)
-                setTimeout(resetCards,1000)
-            } 
+            if (isMatch(id)) {
+                setSolved([...solved, flipped[0], id])
+                resetCards()
+            } else {
+                setClickCounter(clickCounter + 1)
+                setTimeout(resetCards, 1000)
+            }
 
         }
     }
-const sameCard=(id)=>{flipped.includes(id)}
-const isMatch=(id)=>{
-    const clickedCard=cards.find(card=>card.id===id)
-    const flippedCard=cards.find(card=>card.id===flipped[0])
-    return clickedCard.type===flippedCard.type
-}
-const resetCards=()=>{
-    setFlipped([])//ha talalt volt kiuritjuk a tombot ujra
-    setDisabled(false)
-}
-const preloadImages=()=>{
-    cards.map(card=>{
-        const src=card.type
-        new Image().src=src
-        console.log(src)
-    })
-}
+    const sameCard = (id) => { flipped.includes(id) }
+    const isMatch = (id) => {
+        const clickedCard = cards.find(card => card.id === id)
+        const flippedCard = cards.find(card => card.id === flipped[0])
+        return clickedCard.type === flippedCard.type
+    }
+    const resetCards = () => {
+        setFlipped([])//ha talalt volt kiuritjuk a tombot ujra
+        setDisabled(false)
+    }
+    const preloadImages = () => {
+        cards.map(card => {
+            const src = card.type
+            new Image().src = src
+            console.log(src)
+        })
+    }
 
-  return(
-      <div className="container  border shadow p-2">
-            <div className="row justify-content-center pt-2 "> 
+    return (
+        <div className="container  border shadow p-2">
+            <div className="row justify-content-center pt-2 ">
                 <h5 className="p-2" id="visits">Game size: </h5>
-                <select className="custom-select text-center w-25" value={gameSize} onChange={e=>setGameSize(e.target.value)}>
-                   {cardNumber.map(nr => <option  value={nr}>{nr}x{nr}</option>)}        
+                <select className="custom-select text-center w-25" value={gameSize} onChange={e => setGameSize(e.target.value)}>
+                    {cardNumber.map(nr => <option value={nr}>{nr}x{nr}</option>)}
                 </select>
                 <div className="btn btn-outline-danger ml-2 mb-2"> Mistakes: <b>{clickCounter}</b></div>
             </div>
@@ -100,5 +101,5 @@ const preloadImages=()=>{
                 dimension={dimension}
             />
         </div>
-     )
-  }
+    )
+}
